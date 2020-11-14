2ff004461b1b6d81cc4ceb89df74b8e9320cf44c
osamuch <s.hama33@suou.waseda.jp>
2020-11-14 22:45:35 +0900

    本文の変更

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'これはテスト用の変更です:',
             ),
             Text(
               '$_counter',
